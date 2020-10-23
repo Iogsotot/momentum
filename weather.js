49224922df9a6e49809e2e67e87ad6da8430d291
Iogsotot <ajustusa@gmail.com>
2020-10-23 11:18:04 +0300

    fix: style fixes

--- weather.js
+++ weather.js
@@ -7,8 +7,8 @@ const city = document.querySelector('.city');
 
 async function getWeather() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=6ba25fbc62d3d73369c6be9caf83a8d7&units=metric`;
-    const res = await fetch(url);
-    const data = await res.json();
+    const promise = await fetch(url);
+    const data = await promise.json();
 
     weatherIcon.className = 'weather-icon owf';
     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
@@ -17,6 +17,11 @@ async function getWeather() {
     humidity.textContent = `Относительная влажность ${data.main.humidity.toFixed(0)}%`;
     weatherDescription.textContent = data.weather[0].description;
 }
+
+// try {
+//     res = await fetch(url)}
+//  catch (e) { ...} 
+
 
 // Wind name
 function getDirection(angle) {
